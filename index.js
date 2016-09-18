f2f18a1a77d7a8865dd53b2d017a0c94acdb9c13
bredele <olivier.wietrich@gmail.com>
2016-09-18 17:35:04 -0600

    added simple web server

--- index.js
+++ index.js
@@ -3,7 +3,22 @@
  */
 
 var vomit = require('vomit')
+var http = require('http')
 
+
+/**
+ * Create web server.
+ */
+
+http.createServer((req, res) => {
+  if(req.url == '/events') {
+    res.writeHead(200, {'Content-Type': 'text/event-stream' });
+    //res.write('data: ' + Date.now() + '\n\n');
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    html('test').pipe(res)
+  }
+}).listen(3000)
 
 
 /**
@@ -13,7 +28,20 @@ var vomit = require('vomit')
  * @api private
  */
 
-function html() {
+function html(title, component) {
   return vomit`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>${title}</title>
+      <link rel="stylesheet" href="vomit-live.css">
+      <link rel="stylesheet" href="${title}.css">
+    </head>
+    <body>
+    ${component}
+    <script src="bundle.js"></script>
+    </body>
+  </html>
   `
 }
