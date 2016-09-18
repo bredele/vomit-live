a3ee148ce6d558589ef2dd4a259d6ec4fb9ff6d9
bredele <olivier.wietrich@gmail.com>
2016-09-18 16:57:20 -0600

    initialize package

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Server dependencies.
+ */
+
+var vomit = require('vomit')
+
+
+
+/**
+ * Create web server stream.
+ *
+ * @return {Stream}
+ * @api private
+ */
+
+function html() {
+  return vomit`
+  `
+}
